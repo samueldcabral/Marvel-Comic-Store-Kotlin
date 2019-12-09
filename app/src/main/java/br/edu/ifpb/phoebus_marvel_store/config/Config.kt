14a2f9bcae5a0827efc9fd84017178980822cb03
samuel <samueldcabral@gmail.com>
2019-12-08 23:18:57 -0300

    package config added with public and private keys yet to be set

---
+++ app/src/main/java/br/edu/ifpb/phoebus_marvel_store/config/Config.kt
@@ -0,0 +1,12 @@
+package br.edu.ifpb.phoebus_marvel_store.config
+
+open class Config {
+
+    class Keys{
+        companion object {
+            const val PUBLIC_KEY = "YOUR_PUBLIC_KEY"
+            const val PRIVATE_KEY = "YOUR_PRIVATE_KEY"
+        }
+    }
+
+}
